6a4525640fa38cb8db6a7972eebeafb4640dc361
zhuchenxi <973829691@qq.com>
2015-10-07 14:56:29 +0800

    更新生命周期和view拓展的注入方式

---
+++ beam/src/main/java/com/jude/beam/bijection/ActivityLifeCycleDelegateProvider1.java
@@ -0,0 +1,10 @@
+package com.jude.beam.bijection;
+
+import android.app.Activity;
+
+/**
+ * Created by zhuchenxi on 15/10/6.
+ */
+public interface ActivityLifeCycleDelegateProvider1 {
+    ActivityLifeCycleDelegate createActivityLifeCycleDelegate(Activity activity);
+}
