06db31eb7a4f6f146de1e2baf716214464769b53
Jude95 <973829691@qq.com>
2015-10-07 14:59:19 +0800

    增加ViewExpansion的默认设置

--- beam/src/main/java/com/jude/beam/bijection/ActivityLifeCycleDelegateProvider.java
+++ beam/src/main/java/com/jude/beam/bijection/ActivityLifeCycleDelegateProvider.java
@@ -5,6 +5,6 @@ import android.app.Activity;
 /**
  * Created by zhuchenxi on 15/10/6.
  */
-public interface ActivityLifeCycleDelegateProvider1 {
+public interface ActivityLifeCycleDelegateProvider {
     ActivityLifeCycleDelegate createActivityLifeCycleDelegate(Activity activity);
 }
