afbbf3730860aa339875807a3827549213369ea2
Jude95 <973829691@qq.com>
2015-10-24 16:34:11 +0800

    测试

--- demo/src/main/java/com/jude/beamdemo/view/QuestionActivity.java
+++ demo/src/main/java/com/jude/beamdemo/view/QuestionActivity.java
@@ -6,6 +6,7 @@ import android.view.ViewGroup;
 
 import com.jude.beam.bijection.RequiresPresenter;
 import com.jude.beam.expansion.list.BeamListActivity;
+import com.jude.beam.expansion.list.ListConfig;
 import com.jude.beamdemo.model.bean.Question;
 import com.jude.beamdemo.presenter.QuestionPresenter;
 import com.jude.easyrecyclerview.adapter.BaseViewHolder;
@@ -13,8 +14,13 @@ import com.jude.easyrecyclerview.adapter.BaseViewHolder;
 /**
  * Created by zhuchenxi on 15/6/7.
  */
-    @RequiresPresenter(QuestionPresenter.class)
-    public class QuestionActivity extends BeamListActivity<QuestionPresenter,Question> {
+@RequiresPresenter(QuestionPresenter.class)
+public class QuestionActivity extends BeamListActivity<QuestionPresenter,Question> {
+
+    @Override
+    protected ListConfig getConfig() {
+        return super.getConfig().setLoadmoreAble(false);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
